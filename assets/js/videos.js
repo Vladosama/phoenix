f4afd49297318258ba09ad916b82022e3c20bdaf
wubiao03 <wubiao03@taikanglife.com>
2018-05-10 10:10:16 +0800

    引入 VUE 优化 videos 列表

---
+++ assets/js/videos.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        videos: [
+            { title: "凯爹王者时刻 by Sue", img: "assets/videos/kai-sue.JPG", src: "http://v.youku.com/v_show/id_XMzQwMjIzNTM5Ng==.html?spm=a2h3j.8428770.3416059.1"},
+            { title: "亚瑟王者时刻 by 行者都督", img: "assets/videos/yase-wang.JPG", src: "http://v.youku.com/v_show/id_XMzM5NjM5NjcxNg==.html?spm=a2h3j.8428770.3416059.1"},
+            { title: "墨子王者时刻 by 木叶西凉", img: "assets/videos/mozi-sun.JPG", src: "http://v.youku.com/v_show/id_XMzM5NjM5NDk3Ng==.html?spm=a2h3j.8428770.3416059.1"},
+            { title: "AD梦奇王者时刻 by Vladosama", img: "assets/videos/mengqi-vladosama.JPG", src: "http://v.youku.com/v_show/id_XMzM5NjM5NDM1Mg==.html?spm=a2h3j.8428770.3416059.1"},
+            { title: "虞姬王者时刻 by Vladosama", img: "assets/videos/yuji-vladosama.JPG", src: "http://v.youku.com/v_show/id_XMzM5NjM4MzM2OA==.html?spm=a2h3j.8428770.3416059.1"}
+        ]
+    }
+})
